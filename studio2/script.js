7ee1701525baa826e2e7ca60e36e1943056566c8
Rheiana Cuevas <rmcuevas@ucdavis.edu>
2023-02-23 11:55:42 -0800

    need to figure out image map

--- studio2/script.js
+++ studio2/script.js
@@ -4,4 +4,20 @@
     "use strict";
     console.log("reading js");
 
+    const p = document.querySelector("#find-coords");
+    let html = "";
+
+        document.addEventListener("mousemove", reportPos);
+        
+        function reportPos(event) {
+            const xPos = event.clientX;
+            const yPos = event.clientY;
+            html = `The mouse is ${xPos} pixels from the left and ${yPos} pixels from the top`;
+            p.innerHTML = html;
+        }
+
+    const myTop = document.querySelector("area");
+    myTop.addEventListener("click", function(){
+        alert("You clicked the top");
+    });
 }()); //close iife
